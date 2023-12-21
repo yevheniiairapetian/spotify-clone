d87dc85952a3731121b742a7ab02052ca9b4028b
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-12-21 18:01:54 +0100

    feat:update app.js
    Add url access token extraction logic to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Login from './Login-Component/Login';
+import React, { useEffect, useState } from "react";
+import { getTokenFromUrl } from "./Spotify/Spotify";
 
+// app component
 function App() {
+  // setting users access token to access the app with useState
+  const [token, setToken] = useState();
+
+  // get access token from url and hide it. Is done when the app loads
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      // setting the token to useState
+      setToken(_token);
+    }
+
+    console.log("token", token);
+  }, []);
   return (
     <div className="App">
-      <Login/>
+    {/* if user is logged in, show player. otherwise, show login page */}
+      {token ? <h1>Logged in</h1> : <Login />}
     </div>
   );
 }
