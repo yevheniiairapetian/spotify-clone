c89ca32de2c15d41c0bc3c7eda019705b53a9e45
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-12-21 18:35:52 +0100

    feat: add react context and reducer logic to app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Login from './Login-Component/Login';
 import React, { useEffect, useState } from "react";
 import { getTokenFromUrl } from "./Spotify/Spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 const spotify = new SpotifyWebApi();
 // app component
 function App() {
+ 
   // setting users access token to access the app with useState
-  const [token, setToken] = useState();
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   // get access token from url and hide it. Is done when the app loads
   useEffect(() => {
@@ -16,14 +19,35 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      // setting the token to useState
-      setToken(_token);
-      // setting the token to access spotify services
-      spotify.setAccessToken(_token);
+      // setting the token
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
     }
 
-    console.log("token", token);
-  }, []);
+    console.log("[token]", token);
+    spotify.setAccessToken(_token);
+    spotify.getMe().then((user) => {
+      dispatch({
+        type: "SET_USER",
+        user,
+      });
+    });
+    spotify.getUserPlaylists().then((playlists) => {
+      dispatch({
+        type: "SET_PLAYLISTS",
+        playlists,
+      });
+    });
+    spotify.getPlaylist("37i9dQZF1E34Ucml4HHx1w").then((playlist) => {
+      dispatch({
+        type: "SET_DISCOVER_WEEKLY",
+        discover_weekly: playlist,
+      });
+    });
+  }
+, []);
   return (
     <div className="App">
     {/* if user is logged in, show player. otherwise, show login page */}
