69a55609750220c5e205cc75906201e4d07ebffe
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-12-21 15:24:09 +0100

    feat: update login component
    Add logo and login link to lojin.jsx. Add basic styles to login.css

--- src/Login-Component/Login.jsx
+++ src/Login-Component/Login.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./Login.css";
 
 function Login() {
-  return <div className="login"></div>;
+  return <div className="login">
+    <img
+        src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
+        alt="Spotify logo"
+      />
+      <a href="#">LOGIN WITH SPOTIFY</a>
+  </div>;
 }
 
 export default Login;
